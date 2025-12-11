954b061d58f6190c219b526c33af8fd0055655fe
Satyam Kumar <b22236@students.iitmandi.ac.in>
2025-12-11 12:59:04 +0530

    Complete task skeleton

---
+++ task/task.go
@@ -0,0 +1,38 @@
+package task
+
+import (
+"github.com/google/uuid"
+"github.com/docker/go-connections/nat"
+"time"
+)
+
+type Task struct {
+    ID            uuid.UUID
+    Name          string
+    State         State
+    Image         string
+    Memory        int
+    Disk          int
+    ExposedPorts  nat.PortSet
+    PortBindings  map[string]string
+    RestartPolicy string
+    StartTime     time.Time
+    FinishTime    time.Time
+}
+
+type TaskEvent struct {
+    ID        uuid.UUID
+    State     State
+    Timestamp time.Time
+    Task      Task
+}
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled 
+	Running
+	Completed 
+	Failed
+)
