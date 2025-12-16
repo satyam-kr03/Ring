e622e86229c0ec9480c2066bee81b3c4df6cfc8e
Satyam Kumar <b22236@students.iitmandi.ac.in>
2025-12-16 23:09:00 +0530

    Define Run and Stop functions

--- task/task.go
+++ task/task.go
@@ -63,3 +63,97 @@ const (
 	Completed 
 	Failed
 )
+
+/*
+The signature of the ContainerCreate method from the Docker client.
+
+func (cli *Client) ContainerCreate(
+    ctx context.Context,
+    config *container.Config,
+    hostConfig *container.HostConfig,
+    networkingConfig *network.NetworkingConfig,
+    platform *specs.Platform,
+    containerName string) (container.ContainerCreateCreatedBody, error)
+*/
+
+func (d *Docker) Run() DockerResult {
+    ctx := context.Background()
+    reader, err := d.Client.ImagePull(
+        ctx, d.Config.Image, types.ImagePullOptions{})
+    if err != nil {
+        log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+        return DockerResult{Error: err}
+    }
+    io.Copy(os.Stdout, reader)
+
+    rp := container.RestartPolicy{
+        Name: d.Config.RestartPolicy,
+    }
+ 
+    r := container.Resources{
+        Memory: d.Config.Memory,
+        NanoCPUs: int64(d.Config.Cpu * math.Pow(10, 9)),
+    }
+ 
+    cc := container.Config{
+        Image: d.Config.Image,
+        Tty: false,
+        Env: d.Config.Env,
+        ExposedPorts: d.Config.ExposedPorts,
+    }
+ 
+    hc := container.HostConfig{
+        RestartPolicy: rp,
+        Resources:     r,
+        PublishAllPorts: true,
+    }
+
+    resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+    if err != nil {
+        log.Printf("Error creating container using image %s: %v\n", d.Config.Image, err)
+        return DockerResult{Error: err}
+    }
+ 
+    err = d.Client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+    if err != nil {
+        log.Printf("Error starting container %s: %v\n", resp.ID, err)
+        return DockerResult{Error: err}
+    }
+
+    d.Config.Runtime.ContainerID = resp.ID
+ 
+    out, err := cli.ContainerLogs(
+        ctx,
+        resp.ID,
+        types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true}
+    )
+
+    if err != nil {
+        log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+        return DockerResult{Error: err}
+    }
+ 
+    stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+    return DockerResult{ContainerId: resp.ID, Action: "start", Result: "success"}
+}
+
+func (d *Docker) Stop(id string) DockerResult {
+    log.Printf("Attempting to stop container %v", id)
+    ctx := context.Background()
+    err := d.Client.ContainerStop(ctx, id, nil)
+    if err != nil {
+        log.Printf("Error stopping container %s: %v\n", id, err)
+        return DockerResult{Error: err}
+    }
+ 
+    err = d.Client.ContainerRemove(ctx, id, types.ContainerRemoveOptions{
+        RemoveVolumes: true,
+        RemoveLinks:   false,
+        Force:         false,
+    })
+    if err != nil {
+        log.Printf("Error removing container %s: %v\n", id, err)
+        return DockerResult{Error: err}
+    }
+    return DockerResult{Action: "stop", Result: "success", Error: nil}
+}
